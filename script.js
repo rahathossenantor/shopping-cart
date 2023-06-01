f31539b70310c5885c99cf78e7754077eaa7a62e
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-06-01 12:13:55 +0600

    added functionality to handle phone

--- script.js
+++ script.js
@@ -3,17 +3,9 @@ function getElement(id = ""){
     return document.getElementById(id);
 }
 
-// Adding event listenars on case buttons
-getElement("case-plus-btn").addEventListener("click", function(){
-    handleProduct(true)
-});
-
-getElement("case-minus-btn").addEventListener("click", function(){
-    handleProduct(false)
-});
-
-function handleProduct(isIncrease) {
-    let caseQuantity = getElement("case-quatity");
+// Function to handle products
+function handleCase(isIncrease){
+    let caseQuantity = getElement("case-quantity");
     caseQuantity = parseInt(caseQuantity.value);
     if (isIncrease) {
         caseQuantity++;
@@ -21,8 +13,23 @@ function handleProduct(isIncrease) {
     if (isIncrease === false && caseQuantity > 0) {
         caseQuantity--;
     }
-    getElement("case-quatity").value = caseQuantity;
+    getElement("case-quantity").value = caseQuantity;
 
     let casePrice = 59 * caseQuantity;
     getElement("case-price").innerHTML = casePrice;
+}
+
+function handlePhone(isIncrease){
+    let phoneQuantity = getElement("phone-quantity");
+    phoneQuantity = parseInt(phoneQuantity.value);
+    if (isIncrease) {
+        phoneQuantity++;
+    }
+    if (isIncrease === false && phoneQuantity > 0) {
+        phoneQuantity--;
+    }
+    getElement("phone-quantity").value = phoneQuantity;
+
+    let phonePrice = 1219 * phoneQuantity;
+    getElement("phone-price").innerHTML = phonePrice;
 }
