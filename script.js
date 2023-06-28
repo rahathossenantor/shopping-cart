2257b74207c0e8e650c1d7233de76c70bc2ab3c7
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-06-28 22:55:14 +0600

    final version

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 // Shorted version of getElementById()
 function getElement(id = ""){
     return document.getElementById(id);
+};
+
+
+function getInputValue(id) {
+    return parseInt(getElement(id).value);
 }
 
-// Function to handle products
-function handleCase(isIncrease){
-    let caseQuantity = getElement("case-quantity");
-    caseQuantity = parseInt(caseQuantity.value);
+
+function calculateTotal() {
+    const phoneQuantity = getInputValue("phone-quantity");
+    const caseQuantity = getInputValue("case-quantity");
+
+    const subtotal = (phoneQuantity * 1219) + (caseQuantity * 59);
+    const tax = Math.round(subtotal * 0.1);
+    
+    getElement("subtotal").innerHTML = subtotal;
+    getElement("tax").innerHTML = tax;
+    getElement("grand-total").innerHTML = subtotal + tax;
+}
+
+
+// Handling cart
+function handleProduct(product, isIncrease) {
+    const productInput = getElement(`${product}-quantity`);
+    let productQuantity = parseInt(productInput.value);
+    
     if (isIncrease) {
-        caseQuantity++;
-    }
-    if (isIncrease === false && caseQuantity > 0) {
-        caseQuantity--;
-    }
-    getElement("case-quantity").value = caseQuantity;
+        productQuantity++;
+    } else if (isIncrease === false && productQuantity > 1) {
+        productQuantity--;
+    };
+    productInput.value = productQuantity;
 
-    let casePrice = 59 * caseQuantity;
-    getElement("case-price").innerHTML = casePrice;
-}
-
-function handlePhone(isIncrease){
-    let phoneQuantity = getElement("phone-quantity");
-    phoneQuantity = parseInt(phoneQuantity.value);
-    if (isIncrease) {
-        phoneQuantity++;
-    }
-    if (isIncrease === false && phoneQuantity > 0) {
-        phoneQuantity--;
-    }
-    getElement("phone-quantity").value = phoneQuantity;
-
-    let phonePrice = 1219 * phoneQuantity;
-    getElement("phone-price").innerHTML = phonePrice;
-}
+    let productPrice = 0;
+    if (product === "phone") {
+        productPrice = 1219 * productQuantity;
+    } else if (product === "case") {
+        productPrice = 59 * productQuantity;
+    };
+    
+    getElement(`${product}-price`).innerHTML = productPrice;
+    calculateTotal();
+};
